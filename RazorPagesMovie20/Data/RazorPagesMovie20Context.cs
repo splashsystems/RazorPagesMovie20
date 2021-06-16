f6aec365cb745a5105b136b5b2366d22ce031843
Scot <bscot.hagen@gmail.com>
2021-06-16 15:59:25 -0700

    Added Movie model. scaffolded. added migration. updated database.

---
+++ RazorPagesMovie20/Data/RazorPagesMovie20Context.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using RazorPagesMovie20.Models;
+
+namespace RazorPagesMovie20.Data
+{
+    public class RazorPagesMovie20Context : DbContext
+    {
+        public RazorPagesMovie20Context (DbContextOptions<RazorPagesMovie20Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<RazorPagesMovie20.Models.Movie> Movie { get; set; }
+    }
+}
